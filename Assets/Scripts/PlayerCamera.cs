6d15a8f9929076d6776dd990858a05bcc915b312
simone-lopez-zupit <simonpaolo.lopez@zupit.it>
2025-11-15 12:52:43 +0100

    feat(TANR-17): implementing a fantastic camera behaviour

---
+++ Assets/Scripts/PlayerCamera.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class PlayerCamera : MonoBehaviour
+{
+    [SerializeField]
+    private Transform player;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (player != null)
+        {
+            transform.LookAt(player);
+        }
+    }
+}
