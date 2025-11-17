8edf85bbc137c0d78a6c83907b46f254c5cb4379
simonpaolo-lopez-zupit <simonpaolo.lopez@zupit.it>
2025-11-17 21:50:41 +0100

    feat(TANR-24): create test composer scene

---
+++ Assets/Scripts/Composer.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class Composer : MonoBehaviour
+{
+
+    [SerializeField] private GameObject baseElement;
+    [SerializeField] private GameObject bodyElement;
+
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
