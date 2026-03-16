22cfeff6021fb5822bfe915b99b4bfd819be9e3e
simone-lopez-zupit <simonpaolo.lopez@zupit.it>
2026-03-16 17:58:35 +0100

    move logic to punch

--- Assets/Scripts/WeaponSystem/HitBehaviour.cs
+++ Assets/Scripts/WeaponSystem/HitBehaviour.cs
@@ -1,19 +1,13 @@
 using PurrNet.Prediction;
 using UnityEngine;
 
-public class HitBehaviour : PredictedIdentity<HitBehaviour.State>
+public class HitBehaviour : MonoBehaviour
 {
     [SerializeField] private float pushForce = 1000f;
-    private BoxCollider _boxCollider;
     private VehicleWeaponPart _weaponPart;
-
-    private bool _pendingHit;
-    private Vector3 _pendingPushDirection;
-    private PredictedRigidbody _hittedBody;
 
     void Awake()
     {
-        _boxCollider = GetComponent<BoxCollider>();
         _weaponPart = GetComponent<VehicleWeaponPart>();
         if (!_weaponPart)
         {
@@ -21,74 +15,33 @@ public class HitBehaviour : PredictedIdentity<HitBehaviour.State>
         }
     }
 
-    private void FixedUpdate()
+    private void OnTriggerEnter(Collider col)
     {
-        UpdateTriggerState();
-    }
-
-    public struct State : IPredictedData<State>
-    {
-        public bool hitted;
-        public Vector3 pushDirection;
-        public PredictedRigidbody hittedBody;
-
-        public void Dispose() { }
-    }
-
-   
-
-    protected override void Simulate(ref State state, float delta)
-    {
-        UpdateTriggerState();
-
-
-        state.hitted = _pendingHit;
-        state.pushDirection = _pendingPushDirection;
-        state.hittedBody= _hittedBody;
-        if (state.hitted && state.hittedBody)
-        {
-
-            Vector3 dir = state.pushDirection.normalized;
-
-            state.hittedBody.AddForce(dir * pushForce, ForceMode.Impulse);
-        }
-
-        _pendingHit = false;
-        _pendingPushDirection = Vector3.zero;
-        _hittedBody = null;
-
-    }
-
-    private void UpdateTriggerState()
-    {
-        if (!_boxCollider || !_weaponPart)
-        {
-            return;
-        }
-
-        // Trigger when weapon is idle/returning/already consumed its hit window.
-        _boxCollider.isTrigger = !_weaponPart.IsHitting();
-    }
-
-    private void OnCollisionEnter(Collision col)
-    {
-        HandleWeaponContact(col.collider, "OnCollisionEnter");
+        HandleWeaponContact(col, "OnTriggerEnter");
     }
 
     private void HandleWeaponContact(Collider otherCollider, string source)
     {
 
-        if (!otherCollider.CompareTag("Player")) return;
+        if (!otherCollider.CompareTag("PlayerSphere")) return;
         if (!_weaponPart || !_weaponPart.IsHitting()) return;
-        
+
+        PredictedRigidbody hittedBody = otherCollider.GetComponent<PredictedRigidbody>();
+        if (!hittedBody)
+        {
+            hittedBody = otherCollider.GetComponentInParent<PredictedRigidbody>();
+        }
+        if (!hittedBody) return;
+
         Vector3 hitPoint = otherCollider.ClosestPoint(transform.position);
+        Vector3 pushDirection = (otherCollider.transform.position - hitPoint).normalized;
+        if (pushDirection.sqrMagnitude < 0.0001f)
+        {
+            pushDirection = (otherCollider.transform.position - transform.position).normalized;
+        }
 
-        _hittedBody = otherCollider.gameObject.GetComponent<PredictedRigidbody>();
-        if (!_hittedBody) return;
-
-        _pendingHit = true;
-        _pendingPushDirection = (transform.position - hitPoint).normalized;
+        hittedBody.AddForce(pushDirection * pushForce, ForceMode.Impulse);
         _weaponPart.SetWeaponHitState(0);
-        Debug.Log($"{source}: hit from {otherCollider.gameObject.name}, applying push force in direction {_pendingPushDirection}");
+        Debug.Log($"{source}: hit from {otherCollider.gameObject.name}, applying push force in direction {pushDirection}");
     }
 }
