d70015e413ef4e324d9c07b5ad18e4304e572733
Cataldo Cianciaruso <cianciaruso.cataldo@gmail.com>
2026-03-22 20:43:13 +0100

    skybox fix

--- Assets/Scripts/utils/SkyBoxAnimator.cs
+++ Assets/Scripts/utils/SkyBoxAnimator.cs
@@ -3,10 +3,24 @@ using UnityEngine;
 public class SkyBoxAnimator : MonoBehaviour
 {
     public float speed = 2f;
+    private Material _skyboxInstance;
+
+    void Start()
+    {
+        // Crea una copia in memoria: l'asset su disco non viene mai toccato
+        _skyboxInstance = new Material(RenderSettings.skybox);
+        RenderSettings.skybox = _skyboxInstance;
+    }
 
     void Update()
     {
-        float rot = Time.time * speed;
-        RenderSettings.skybox.SetFloat("_Rotation", rot);
+        _skyboxInstance.SetFloat("_Rotation", Time.time * speed);
+    }
+
+    void OnDestroy()
+    {
+        // Pulizia: evita memory leak nell'editor
+        if (_skyboxInstance != null)
+            Destroy(_skyboxInstance);
     }
 }
