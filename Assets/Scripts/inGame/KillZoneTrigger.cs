19f11f242bc2ce603226ec0b939cc21680dcfa50
simonpaolo-lopez-zupit <simonpaolo.lopez@zupit.it>
2026-04-19 23:09:19 +0200

    fix checkpoint bug on clients

--- Assets/Scripts/inGame/KillZoneTrigger.cs
+++ Assets/Scripts/inGame/KillZoneTrigger.cs
@@ -4,8 +4,6 @@ public class KillZoneTrigger : MonoBehaviour
 {
     private void OnTriggerEnter(Collider other)
     {
-        if (!other.CompareTag("PlayerSphere")) return;
-
         var player = other.GetComponentInParent<PlayerIdentity>();
         if (player == null || !player.IsLocalOwner) return;
 
