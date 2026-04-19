10fdf60569737c10ae934e71761277f25822af81
simonpaolo-lopez-zupit <simonpaolo.lopez@zupit.it>
2026-04-19 23:47:55 +0200

    cached json

--- Assets/Scripts/managers/GameManager.cs
+++ Assets/Scripts/managers/GameManager.cs
@@ -25,7 +25,7 @@ public class GameManager : MonoBehaviour
 
     private void Start()
     {
-        Application.targetFrameRate = 60;
+        //Application.targetFrameRate = 60;
         QualitySettings.vSyncCount = 0;
     }
 
